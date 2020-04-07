b055ddcaa660b18f7d42f69cc6c5e41db50e8c24
ThallesAguiar <thallesaguiar@github.com>
2020-04-07 16:26:20 -0300

    documentos que faltou

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <Navigation />
+    <LigarBotao/>
+    <router-view />
+    <Rodape/>
+  </div>
+</template>
+
+<script>
+import Navigation from "@/components/Navigation";
+import LigarBotao from "@/components/LigarBotao";
+import Rodape from "@/components/Rodape.vue";
+export default {
+  components: {
+    Navigation,
+    LigarBotao,
+    Rodape
+  }
+};
+</script>
+
+<style lang="scss">
+html, body{
+  padding: 0;
+  margin: 0;
+  background-image:url("./assets/fundo.jpg");
+  background-repeat: repeat;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  // color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
